fd5b6867936054018374a4d5ac3c2619025a381b
Surajv <vsuraj2405@gmail.com>
2022-09-07 19:42:33 +0530

    updated social credentials

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Suraj's Learningletters`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Suraj Verma`,
+      summary: `(Software Engineer)`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
     social: {
-      twitter: `kylemathews`,
+      link: `srj_v`,
     },
   },
   plugins: [
